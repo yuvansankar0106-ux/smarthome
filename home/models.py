6dce5310ade1c3b7a51e48f7210661b27c0f2398
Yuvan Sankar <yuvansankar0106-ux@github.com>
2026-09-18 19:42:59 +0530

    glassy fixed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    value = models.CharField(max_length=50, default="OFF")
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 def login_view(request):
-    if request.method == 'POST':
-        if request.POST['username']=='student' and request.POST['password']=='1234':
-            request.session['user']='student'
-            return redirect('dashboard')
     return render(request, 'login.html')
 
 def dashboard(request):
+    if request.method == 'POST':
+        return JsonResponse({"status":"ok"})
     return render(request, 'dashboard.html')
 
-def security(request):
-    if request.method == 'POST':
-        request.session['alert'] = request.POST.get('alert')
-        request.session['camera'] = request.POST.get('camera')
-        return render(request, 'security.html', {'msg':'Alert Saved!'})
-    return render(request, 'security.html', {
-        'alert': request.session.get('alert','No Intrusion'),
-        'camera': request.session.get('camera','ON')
-    })
-
-def devices(request):
-    if request.method == 'POST':
-        request.session['light'] = request.POST.get('light')
-        request.session['door'] = request.POST.get('door')
-        return render(request, 'devices.html', {'msg':'Devices Saved!'})
-    return render(request, 'devices.html', {
-        'light': request.session.get('light','OFF'),
-        'door': request.session.get('door','LOCKED')
-    })
+def verify_otp_view(request):
+    return redirect('/')
+def logout_view(request):
+    return redirect('/login/')
